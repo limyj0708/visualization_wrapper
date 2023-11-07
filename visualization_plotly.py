90cff80d3cf7c52f2181883ae2e20962d6d7b9aa
limyj0708 <limyj0708@gmail.com>
2023-11-07 10:47:36 +0900

    modify visualization_plotly.py

--- visualization_plotly.py
+++ visualization_plotly.py
@@ -19,10 +19,10 @@ class CompareDistribution:
         #             If False, perform Welch’s t-test, which does not assume equal population variance [2].
         ks_2samp_result = ks_2samp(data_1, data_2)
 
-        hist_data = [data_1, data_2]
+        hist_data = [data_2, data_1]
         group_labels = list_dict_keys
 
-        fig = ff.create_distplot(hist_data, group_labels, bin_size=bin_size, colors=['red', 'blue'], show_rug=False)
+        fig = ff.create_distplot(hist_data, group_labels, bin_size=bin_size, colors=['blue', 'red'], show_rug=False)
         fig.update_layout(
             barmode='overlay'
             , title = {
@@ -44,18 +44,18 @@ class CompareDistribution:
         fig2 = go.Figure()
         fig2.add_trace(
             go.Box(
-                  x = data_2
-                , y = [key_2] * len(data_2)
-                , name = key_2
-                , marker_color = 'blue'
-            )
-        )
-        fig2.add_trace(
-            go.Box(
                   x = data_1
                 , y = [key_1] * len(data_1)
                 , name = key_1
                 , marker_color = 'red'
+            )
+        )
+        fig2.add_trace(
+            go.Box(
+                  x = data_2
+                , y = [key_2] * len(data_2)
+                , name = key_2
+                , marker_color = 'blue'
             )
         )
 
